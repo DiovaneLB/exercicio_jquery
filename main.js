10629c988991a9153a851da030aa79203ad61896
Diovane Braga <diovanelopesbraga1996@gmail.com>
2023-10-10 13:57:56 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const listaTarefas = document.getElementById("lista-tarefas");
+  const novaTarefaInput = document.getElementById("nova-tarefa");
+  const adicionarTarefaBotao = document.getElementById("adicionar-tarefa");
+
+  adicionarTarefaBotao.addEventListener("click", function () {
+      const novaTarefaTexto = novaTarefaInput.value.trim();
+
+      if (novaTarefaTexto !== "") {
+          const novaTarefaItem = document.createElement("li");
+          novaTarefaItem.textContent = novaTarefaTexto;
+
+          novaTarefaItem.addEventListener("click", function () {
+              novaTarefaItem.classList.toggle("ok"); 
+          });
+
+          listaTarefas.appendChild(novaTarefaItem);
+
+          novaTarefaInput.value = "";
+      }
+  });
+});
